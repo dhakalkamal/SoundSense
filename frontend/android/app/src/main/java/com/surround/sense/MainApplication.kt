4cc67427b834ff61d1bd3436c4afa40297ce7772
Vyanktesh <aralivyankatesh@gmail.com>
2026-05-29 11:40:51 -0400

    fix: hardcode isNewArchEnabled=false, remove load() call to fix DSO crash

--- frontend/android/app/src/main/java/com/surround/sense/MainApplication.kt
+++ frontend/android/app/src/main/java/com/surround/sense/MainApplication.kt
@@ -6,7 +6,6 @@ import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
@@ -25,7 +24,7 @@ class MainApplication : Application(), ReactApplication {
 
         override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
-        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+        override val isNewArchEnabled: Boolean = false
         override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
       }
 
@@ -35,9 +34,7 @@ class MainApplication : Application(), ReactApplication {
   override fun onCreate() {
     super.onCreate()
     SoLoader.init(this, false)
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      load()
-    }
+    // New Architecture disabled — libreact_featureflagsjni.so not bundled in this build
     ApplicationLifecycleDispatcher.onApplicationCreate(this)
   }
 }
